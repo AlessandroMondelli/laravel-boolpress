de378e24829a06a97661dcd5c62132fb911dc67a
Alessandro_Mondelli <alexs98@hotmail.it>
2020-02-21 16:28:07 +0100

    Corretta navbar pubblica

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.public')
 
 @section('content')
-    <div class="container">
+    <div class="container" id="cont-posts">
         <div class="row">
-            <div class="col-lg-12 videogame-card">
+            <div class="col-lg-6 videogame-card">
                 <h1>Lista posts pubblica</h1>
+            </div>
+            <div class="col-lg-6">
+                <a class="btn btn-success float-right" href="{{ route('public.home') }}">Home</a>
             </div>
         </div>
         <div class="row">
