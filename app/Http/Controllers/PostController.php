104fb337ca619f94bd964ed151de82471886ce21
Alessandro_Mondelli <alexs98@hotmail.it>
2020-02-24 17:13:37 +0100

    Cliccando la categoria su un post, verranno mostrati tutti i post con quella categoria

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,19 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\Category;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index()
-    {
+    public function index() {
         $posts = Post::all(); //Prendo tutti i posts
         return view('posts',['posts'=>$posts]);
     }
 
-    public function show($slug)
-    {
+    public function show($slug) {
         $post = Post::where('slug',$slug)->first();
         return view('choosen-post',['post' => $post]);
+    }
+
+    public function postCategoria($slug) {
+        $category = Category:: where('slug',$slug)->first(); //Recupero categoria
+        if (!empty($category)) { //Se non è null
+            $category_post = $category->posts; //Prendo categoria post
+            return view('single-category',['category'=> $category, 'posts' => $category_post]);
+        } else {
+            return abort(404); //Se non trovo categorie, riporto un errore
+        }
     }
 }
--- resources/views/single-category.blade.php
+++ resources/views/single-category.blade.php
@@ -3,26 +3,31 @@
 @section('content')
     <div class="container cont-posts">
         <div class="row">
-            <div class="col-lg-12">
-                <h1>Visualizzazione Post</h1>
+            <div class="col-lg-6">
+                <h1>Lista posts con categoria {{ $category->name }}</h1>
+            </div>
+            <div class="col-lg-6">
+                <a class="btn btn-success float-right" href="{{ route('public.home') }}">Home</a>
             </div>
         </div>
         <div class="row">
+            @forelse ($posts as $post)
             <div class="col-lg-12 post-card">
                 <div class="card">
                   <div class="card-body">
                     <h2><p class="card-title">{{ $post->title }}</p></h2>
-                    <img class="cover_img" src="{{ $post->cover_image ? asset('storage/' . $post->cover_image) : asset('storage/uploads/notAvailable.png') }}" alt=" Immagine di {{ $post->title }}">
                     <p class="card-text">{{ $post->content }}</p>
                     @if (!empty($post->category))
-                        <p class="card-text">Categoria: <a href="{{ route('blog.category',['slug' => $post->category->slug]) }}">{{ $post->category->name }}</a></p>
+                        <p class="card-text">Categoria: {{ $post->category->name }}</p>
                     @endif
                     <p class="card-text">Autore: {{ $post->author }}</p>
-                    <p class="card-text">Creato: {{ $post->created_at }}</p>
-                    <p class="card-text">Aggiornato: {{ $post->updated_at }}</p>
+                    <a href="{{ route('blog.show',['slug'=>$post->slug]) }}" class="btn btn-primary">Dettagli</a>
                   </div>
                 </div>
             </div>
+        @empty
+            <p>Non è presente alcun Post</p>
+        @endforelse
         </div>
     </div>
 @endsection
